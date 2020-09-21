8f9514650ef2f635fb98c56e724e5b194968879f
sirily11 <sirily1997@gmail.com>
2020-09-22 04:26:42 +0800

    updated name

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from docs_generator.docs_generator import convert_notebook_to_html
 from shutil import copytree
 
 if __name__ == '__main__':
-    convert_notebook_to_html()
+    convert_notebook_to_html(site_name="Class Notes")
     print("Copy index.md")
     with open('README.md', 'r') as f:
         content = f.read()

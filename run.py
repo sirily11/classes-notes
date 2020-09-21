83edc664d5c58019d769e87849eefc808e57a0fa
sirily11 <sirily1997@gmail.com>
2020-09-22 00:18:06 +0800

    first commit

---
+++ run.py
@@ -0,0 +1,10 @@
+from docs_generator.docs_generator import convert_notebook_to_html
+from shutil import copytree
+
+if __name__ == '__main__':
+    convert_notebook_to_html()
+    print("Copy index.md")
+    with open('README.md', 'r') as f:
+        content = f.read()
+        with open('docs/index.md', 'w') as f1:
+            f1.write(content)

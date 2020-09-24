35c84bacc47a157be6bb814fdd597f491687716d
sirily11 <sirily1997@gmail.com>
2020-09-24 22:20:18 +0800

    new screenshot

--- screenshot_ocr/screenshot.py
+++ screenshot_ocr/screenshot.py
@@ -1,15 +1,49 @@
-import mss
-import tkinter as tk
-from PIL import Image
+import time
+
 import pytesseract
-import mss.tools
-from screenshot_ocr.gui import Application
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler, FileCreatedEvent
+from pathlib import Path
+from os.path import join, splitext
 
 
+class OnMyWatch:
+    # Set the directory on watch 
+
+    def __init__(self):
+        self.observer = Observer()
+        home = str(Path.home())
+        self.watchDirectory = join(home, 'Desktop', 'screenshots')
+
+    def run(self):
+        event_handler = Handler()
+        self.observer.schedule(event_handler, self.watchDirectory, recursive=True)
+        self.observer.start()
+        try:
+            while True:
+                time.sleep(5)
+        except:
+            self.observer.stop()
+            print("Observer Stopped")
+
+        self.observer.join()
+
+
+class Handler(FileSystemEventHandler):
+
+    def on_created(self, event: FileCreatedEvent):
+        filename = event.src_path
+
+        if not event.is_directory:
+            new_filename, ext = splitext(filename)
+            if ext in ['.png', '.jpg']:
+                result = pytesseract.image_to_string(filename, lang='eng')
+                new_filename += '.txt'
+                with open(new_filename, 'w+') as f:
+                    f.write(result)
+                    print(f"Write result to the file {new_filename}")
 
 
 if __name__ == '__main__':
-    root = tk.Tk()
-    root.minsize(300, 400)
-    app = Application(master=root)
-    app.mainloop()
+    watch = OnMyWatch()
+    watch.run()
